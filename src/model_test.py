dd4d51bc1e969065cc0842c6d4b1acb47f910337
elherold <103029274+elherold@users.noreply.github.com>
2024-04-03 12:50:37 +0200

    update

--- src/model_test.py
+++ src/model_test.py
@@ -10,6 +10,7 @@ import pickle
 from merge_train_test import getting_datasets
 import os
 from datetime import datetime
+import tensorflow as tf
 
 
 def create_embedding_matrix(word_index, embedding_dim=100):
@@ -88,57 +89,73 @@ def train_models():
 
     # train a distinct model with each training set
     for name, (X_train, y_train) in train_sets.items():
+        print(f"currently working on model: {name}")
         # Tokenize and pad the training and testing sequences
         X_train = list(X_train)
 
         # This needs to be removed eventually, just skipping it for testing purposes
-        # if name=="original_data" or name=="augmented_synonyms":
-        #     continue
+        if name=="original_data":
+            continue
             
         X_train_padded, X_test_padded, tokenizer, word_index, embedding_matrix = tokenizer_padding(X_train, X_test, name)
 
         y_train = np.array(y_train, dtype=np.int32)
         y_test = np.array(y_test, dtype=np.int32)
 
-        print(f"Currently training model {name}")
-        print("X_train_padded:", X_train_padded[0].shape, X_train_padded[0].dtype)
-        print("y_train:", y_train.shape, y_train.dtype)
-        print("X_test_padded:", X_test_padded[0].shape, X_test_padded[0].dtype)
-        print("y_test:", y_test.shape, y_test.dtype)
+        #print(f"Currently training model {name}")
+        #print("X_train_padded:", X_train_padded[0].shape, X_train_padded[0].dtype)
+        #print("y_train:", y_train.shape, y_train.dtype)
+        #print("X_test_padded:", X_test_padded[0].shape, X_test_padded[0].dtype)
+        #print("y_test:", y_test.shape, y_test.dtype)
 
-        # Create the model
-        model = Sequential()
-        model.add(Embedding(input_dim=len(word_index) + 1, output_dim=100, input_length=500, weights=[embedding_matrix], trainable=False)) # set trainable to False to keep the embeddings fixed
-        model.add(LSTM(100))
-        model.add(Dropout(0.2))
-        model.add(Dense(1, activation='sigmoid'))
+        # Load the model if it already exists
+        try: 
+            model = tf.keras.models.load_model(f'../models/model_{name}')
+            print(f"Model {name} already exists, skipping training")
 
-        # Compile the model
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        except FileNotFoundError:
+            print(f"Model {name} does not exist, training and saving new model")
 
-        # TensorBoard setup
-        log_dir = os.path.join("logs", name, datetime.now().strftime("%Y%m%d-%H%M%S"))
-        tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+            # Create the model
+            model = Sequential()
+            model.add(Embedding(input_dim=len(word_index) + 1, output_dim=100, input_length=500, weights=[embedding_matrix], trainable=False)) # set trainable to False to keep the embeddings fixed
+            model.add(LSTM(100))
+            model.add(Dropout(0.2))
+            model.add(Dense(1, activation='sigmoid'))
 
-        # Train the model
-        history = model.fit(X_train_padded[0], y_train, validation_data=(X_test_padded[0], y_test), epochs=3, batch_size=64)
+            # Compile the model
+            model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+            # TensorBoard setup
+            log_dir = os.path.join("logs", name, datetime.now().strftime("%Y%m%d-%H%M%S"))
+            tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+            # Train the model
+            history = model.fit(X_train_padded, y_train, 
+                                validation_data=(X_test_padded, y_test), 
+                                epochs=3, batch_size=64, 
+                                callbacks=[tensorboard_callback])
+
+            # After training, save the tokenizer and word_index
+            with open(f'../models/tokenizer_{name}.pickle', 'wb') as handle:
+                pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+            with open(f'../models/word_index_{name}.json', 'w') as f:
+                json.dump(word_index, f)
+
+            # save the training history
+            with open(f'../models/history_{name}.json', 'w') as f:
+                json.dump(history.history, f)
+
+            # Save the embedding matrix
+            np.save(f'../models/embedding_matrix_{name}.npy', embedding_matrix)
+
+            # Save the model
+            model.save(f'../models/model_{name}')
+        
         # Evaluate the model
-        loss, accuracy = model.evaluate(X_test_padded[0], y_test)
+        loss, accuracy = model.evaluate(X_test_padded, y_test)
         print(f"Model {name} - Loss: {loss}, Accuracy: {accuracy}")
-
-        # After training, save the tokenizer and word_index
-        with open(f'../models/tokenizer_{name}.pickle', 'wb') as handle:
-            pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-        with open(f'../models/word_index_{name}.json', 'w') as f:
-            json.dump(word_index, f)
-
-        # Save the embedding matrix
-        np.save(f'../models/embedding_matrix_{name}.npy', embedding_matrix)
-
-        # Save the model
-        #model.save(f'../models/model_{name}')
 
 if __name__ == "__main__":
     train_models()
